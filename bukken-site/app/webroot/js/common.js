beddb713aadbe18c83886934b4cd1393ecb8d73a
koshihikari <koshihikari@gmail.com>
2013-10-17 15:40:42 +0900

    メニューのRollOver対応。

--- bukken-site/app/webroot/js/common.js
+++ bukken-site/app/webroot/js/common.js
@@ -1,3 +1,13 @@
 ﻿$(function() {
 	$('.menu-wrapper > div').tile();
+
+	$('.return-to-top').on('click', function(event) {
+		$('body, html').animate(
+			{
+				scrollTop: 0
+			},
+			500
+		);
+		return false;
+	});
 });
