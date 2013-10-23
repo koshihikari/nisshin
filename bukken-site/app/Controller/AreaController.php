fc55cf113185f0ecee44e8f8c6cefbc8f2240b02
koshihikari <koshihikari@gmail.com>
2013-10-23 19:36:06 +0900

    エリア毎のページのcontentsファイルを1つに統合。

--- bukken-site/app/Controller/AreaController.php
+++ bukken-site/app/Controller/AreaController.php
@@ -4,42 +4,55 @@ class AreaController extends AppController {
 	var $layout = 'nisshin-bukken-0.0.1';
 
 	public function beforeFilter() {
-		// $this->Auth->allow('login', 'logout', 'add');
 	}
 
 	public function tokyo() {
 		$this->set(
 			array(
-				'title_for_layout'			=> '東京ページ'
+				'title_for_layout'			=> '東京ページ',
+				'area'						=> 'tokyo'
 			)
 		);
-		$this->render("../Contents/Area/tokyoInArea");
+		$this->render("../Contents/Area/pageInArea");
 	}
 
 	public function kanagawa() {
 		$this->set(
 			array(
-				'title_for_layout'			=> '神奈川ページ'
+				'title_for_layout'			=> '神奈川ページ',
+				'area'						=> 'kanagawa'
 			)
 		);
-		$this->render("../Contents/Area/kanagawaInArea");
+		$this->render("../Contents/Area/pageInArea");
 	}
 
 	public function chiba() {
 		$this->set(
 			array(
-				'title_for_layout'			=> '千葉ページ'
+				'title_for_layout'			=> '千葉ページ',
+				'area'						=> 'chiba'
 			)
 		);
-		$this->render("../Contents/Area/chibaInArea");
+		$this->render("../Contents/Area/pageInArea");
 	}
 
 	public function saitama() {
 		$this->set(
 			array(
-				'title_for_layout'			=> '埼玉ページ'
+				'title_for_layout'			=> '埼玉ページ',
+				'area'						=> 'saitama'
 			)
 		);
-		$this->render("../Contents/Area/saitamaInArea");
+		$this->render("../Contents/Area/pageInArea");
+	}
+
+	public function all() {
+		$this->set(
+			array(
+				'title_for_layout'			=> '全物件ページ',
+				'area'						=> 'all'
+			)
+		);
+		$this->render("../Contents/Area/pageInArea");
 	}
 }
