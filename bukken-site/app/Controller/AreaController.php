9d039725223e3abc9cf402764c67233d690b6b06
koshihikari <koshihikari@gmail.com>
2013-10-24 12:28:54 +0900

    エリア毎ページの物件データをControllerからViewに渡すように修正。

--- bukken-site/app/Controller/AreaController.php
+++ bukken-site/app/Controller/AreaController.php
@@ -7,50 +7,60 @@ class AreaController extends AppController {
 	}
 
 	public function tokyo() {
+		$data = $this->getResidenceData('tokyo');
 		$this->set(
 			array(
 				'title_for_layout'			=> '東京ページ',
-				'area'						=> 'tokyo'
+				'area'						=> 'tokyo',
+				'residence_data'			=> $data
 			)
 		);
 		$this->render("../Contents/Area/pageInArea");
 	}
 
 	public function kanagawa() {
+		$data = $this->getResidenceData('kanagawa');
 		$this->set(
 			array(
 				'title_for_layout'			=> '神奈川ページ',
-				'area'						=> 'kanagawa'
+				'area'						=> 'kanagawa',
+				'residence_data'			=> $data
 			)
 		);
 		$this->render("../Contents/Area/pageInArea");
 	}
 
 	public function chiba() {
+		$data = $this->getResidenceData('chiba');
 		$this->set(
 			array(
 				'title_for_layout'			=> '千葉ページ',
-				'area'						=> 'chiba'
+				'area'						=> 'chiba',
+				'residence_data'			=> $data
 			)
 		);
 		$this->render("../Contents/Area/pageInArea");
 	}
 
 	public function saitama() {
+		$data = $this->getResidenceData('saitama');
 		$this->set(
 			array(
 				'title_for_layout'			=> '埼玉ページ',
-				'area'						=> 'saitama'
+				'area'						=> 'saitama',
+				'residence_data'			=> $data
 			)
 		);
 		$this->render("../Contents/Area/pageInArea");
 	}
 
 	public function all() {
+		$data = $this->getResidenceData('all');
 		$this->set(
 			array(
 				'title_for_layout'			=> '全物件ページ',
-				'area'						=> 'all'
+				'area'						=> 'all',
+				'residence_data'			=> $data
 			)
 		);
 		$this->render("../Contents/Area/pageInArea");
