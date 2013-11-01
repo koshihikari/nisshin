da2be86ea1c93e17eb2c0e81f344cc0e2efa2284
pochi-sato <pochi@satou-no-MacBook-Air.local>
2013-11-01 12:32:04 +0900

    ブラウザ未チェック　コンセプト簡易版完了

--- bukken-site/app/webroot/cake-js/self/contents/concept/concept.js
+++ bukken-site/app/webroot/cake-js/self/contents/concept/concept.js
@@ -1,12 +1,30 @@
 ﻿// $(function() {
 $(window).load(function() {
 	// $('.residence-item').tile(2);
-	$('.area-tokyo23').tile(2);
-	$('.area-tokyoOther').tile(2);
-	$('.area-kanagawa').tile(2);
-	$('.area-kanagawaInAll').tile(2);
-	$('.area-saitama').tile(2);
-	$('.area-saitamaInAll').tile(2);
-	$('.area-chiba').tile(2);
-	$('.area-chibaInAll').tile(2);
+
+
+// var positionNaturally = $('div.detail.naturally').offset().top;
+// var positionPartner = $('div.detail.partner').offset().top;
+// var positionSafety = $('div.detail.safety').offset().top;
+// var positionComfort = $('div.detail.comfort').offset().top;
+
+// alert(positionNaturally)
+
+// $('div.blockMenu_1').on('click',function(){
+// 	$('body,html').animate({scrollTop:positionNaturally}, 4000, 'swing');
+// });
+// $('div.blockMenu_2').on('click',function(){
+// 	$('body,html').animate({scrollTop:positionPartner}, 4000, 'swing');
+// });
+// $('div.blockMenu_3').on('click',function(){
+// 	$('body,html').animate({scrollTop:positionSafety}, 4000, 'swing');
+// });
+// $('div.blockMenu_4').on('click',function(){
+// 	$('body,html').animate({scrollTop:positionComfort}, 4000, 'swing');
+// });
+
+
+
+
+
 });
