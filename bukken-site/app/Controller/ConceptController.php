1ac4ba569271d7ed78ddfe7072ccb605e98ff991
koshihikari <koshihikari@gmail.com>
2013-11-08 16:32:37 +0900

    各ページのtitle、keywords、descriptionを修正。

--- bukken-site/app/Controller/ConceptController.php
+++ bukken-site/app/Controller/ConceptController.php
@@ -10,10 +10,10 @@ class ConceptController extends AppController {
 	public function concept() {
 		$this->set(
 			array(
-				'title_for_layout'			=> 'コンセプト｜日神不動産の住まい『パレステージ』',
-				'keywords'					=> 'マンションマンション,新築マンション,新築分譲マンション,日神不動産,パレステージ',
+				'title_for_layout'			=> 'コンセプト｜日神不動産の住まい',
+				'keywords'					=> 'マンション,新築マンション,新築分譲マンション,日神不動産,パレステージ',
 				'description'				=> '日神不動産のブランドコンセプトページです。私たちは常に、その空間に住まう家族のライフシーンを思い描くことから、新しいマンションの在り方を構想します。日常的な視点から暮らしを見据え、家族をテーマにした永住空間として資質の向上を図っています。',
-				'h1'						=> 'コンセプト｜日神不動産のパレステージ'
+				'h1'						=> 'コンセプト｜日神不動産の新築分譲マンション'
 			)
 		);
 		$this->render("../Contents/Concept/conceptInConcept");
