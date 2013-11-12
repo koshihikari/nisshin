3457fe4cae72608b0978619df2b005be82f80ab9
koshihikari <koshihikari@gmail.com>
2013-11-12 12:39:32 +0900

    トップページアクセス時、UAがiPhone/iPod/Androidの場合、モバイルページに遷移。

--- bukken-site/app/webroot/cake-js/self/contents/top/top.js
+++ bukken-site/app/webroot/cake-js/self/contents/top/top.js
@@ -4,4 +4,11 @@ $(window).load(function() {
 	// $('.cc_resiBox').tile(4);
 	$('.cc_resiName').tile(4);
 	$('.cc_resiTexts').tile(4);
+
+	var ua = navigator.userAgent;
+	if ((ua.indexOf('iPhone') > 0 || ua.indexOf('iPod') > 0 || ua.indexOf('Android') > 0 )) {
+		if(confirm('スマートフォン用のページに遷移しますか？')) {
+			location.href = 'http://ns-mb.jp/';
+		}
+	}
 });
